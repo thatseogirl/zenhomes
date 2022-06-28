3cc71cfc956d216221135e6cfe407b96bf060f0a
NoHop3 <u_sux@abv.bg>
2022-06-28 21:34:03 +0200

    Add rental page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignIn from "./auth/SignIn/SignIn";
 import SignUp from "./auth/SignUp/SignUp";
-import AllRentals from "./pages/AllRentals";
+import {AllRentals} from "./pages/AllRentals";
+import AddRental from "./pages/AddRental";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<Dashboard />} /> */}
         {/* <Route path="/rentals/:rentalId" element={<RentalDetails />} /> */}
-        {/* <Route path="/rentals/add" element={<AddRental />} /> */}
+        <Route path='/rentals/add' element={<AddRental />} />
         <Route path='/rentals' element={<AllRentals />} />
         <Route path='/login' element={<SignIn />} />
         <Route path='/register' element={<SignUp />} />
